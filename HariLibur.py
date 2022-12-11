ea5be4de2dcde610013f983147930a84105785f7
Rosinta Banjarnahor <89386569+Rosinta14@users.noreply.github.com>
2022-12-12 00:39:37 +0700

    Update HariLibur.py

--- HariLibur.py
+++ HariLibur.py
@@ -1,1 +1,18 @@
-
+class HariLibur:
+    def __init__(self,data):
+        self.data=data
+    def show(self):
+        print("Hari :",self.data['holiday_name'])
+        print("Tanggal :",self.data['holiday_date'])
+        if self.data['is_national_holiday']==True:
+            print("Hari Libur!")
+        else:
+            print("Bukan Hari Libur:(")
+        print()
+def menu():
+    print("Hari Nasional!")
+    print("Menu : ")
+    print("1. List hari Nasional 2022")
+    print("2. Cari hari Nasional berdasarkan tanggal")
+    print("3. Cari hari Nasional berdasarkan nama")
+    print("4. Keluar")
